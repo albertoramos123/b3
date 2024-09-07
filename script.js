566a490355c126f0072b7a544d4c6c6ee77f5584
alberto ramos <162235872+albertoramos123@users.noreply.github.com>
2024-09-07 21:45:13 +0000

    add new sub-dropdown

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 // JavaScript para ajustar o intervalo do carrossel
 
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+document.addEventListener('click', function (e) {
+    const dropdowns = document.querySelectorAll('.dropdown, .sub-dropdown');
+    const isClickInside = dropdowns.some(dropdown => dropdown.contains(e.target));
+
+    if (!isClickInside) {
+        dropdowns.forEach(dropdown => dropdown.style.display = 'none');
+    }
+});
+
+document.querySelectorAll('nav ul li').forEach(item => {
+    item.addEventListener('mouseover', function () {
+        this.querySelector('.dropdown, .sub-dropdown')?.style.display = 'block';
+    });
+
+    item.addEventListener('mouseout', function () {
+        this.querySelector('.dropdown, .sub-dropdown')?.style.display = 'none';
+    });
+});
+
